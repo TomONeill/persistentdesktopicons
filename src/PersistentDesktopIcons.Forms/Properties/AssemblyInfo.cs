5e74bd4b466a4245bee4b2eda53a45a1c554e1d6
Tom van de Graaf <tom2835@hotmail.com>
2016-11-18 23:53:26 +0100

    Changed the exe title

--- src/PersistentDesktopIcons.Forms/Properties/AssemblyInfo.cs
+++ src/PersistentDesktopIcons.Forms/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PersistentDesktopIcons.Forms")]
+[assembly: AssemblyTitle("Persistent Desktop Icons")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PersistentDesktopIcons.Forms")]
+[assembly: AssemblyProduct("Persistent Desktop Icons")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
