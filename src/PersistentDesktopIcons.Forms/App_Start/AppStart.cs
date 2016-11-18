831377c62a84a5841a62204456de66bb1cfd4ff1
Tom van de Graaf <tom2835@hotmail.com>
2016-11-18 23:03:46 +0100

    Added Forms application, removed Service and Installer.

---
+++ src/PersistentDesktopIcons.Forms/App_Start/AppStart.cs
@@ -0,0 +1,22 @@
+﻿using PersistentDesktopIcons.Common;
+using System;
+using System.Windows.Forms;
+
+namespace PersistentDesktopIcons.Forms
+{
+    internal static class AppStart
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            new DesktopIconsController().Start();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PersistentDesktopIconsForm());
+        }
+    }
+}
---
+++ src/PersistentDesktopIcons.Forms/PersistentDesktopIconsForm.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace PersistentDesktopIcons.Forms
+{
+    public partial class PersistentDesktopIconsForm : Form
+    {
+        public PersistentDesktopIconsForm()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var runRegistryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            var registerRegistration = runRegistryKey.GetValue(Application.ProductName);
+
+            if (registerRegistration == null)
+            {
+                runRegistryKey.SetValue(Application.ProductName, Application.ExecutablePath.ToString());
+            }
+        }
+    }
+}
